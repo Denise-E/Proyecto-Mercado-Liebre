30a15638c8e9c083dc87bde5ddb6b1a58be769e8
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-01-30 18:00:24 -0300

    haciendo home dinámica

--- src/controller/index.js
+++ src/controller/index.js
@@ -1,6 +1,8 @@
+const products = require("../data/products.json");
 
 module.exports = {
     home: (req, res)=> res.render("home",{
+        products: products,
         styles: ["home"]
     })
 }
