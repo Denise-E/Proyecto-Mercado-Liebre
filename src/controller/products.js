6a5bc848941513be81cd02467512aadd8cce3704
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-02-05 21:26:05 -0300

    seteando ruta dinámica para las categorías

--- src/controller/products.js
+++ src/controller/products.js
@@ -1,11 +1,18 @@
 const model = require("../models/products");
 const products = require("../data/products.json");
+const categories = require("../data/categories.json");
+
 
 module.exports = {
     show: (req, res)=> {
          res.render("products/show",{
             product: products.find(product => product.id == req.params.id),
             styles: ["/products/show"]
-    }) 
-}
+    })},
+    categories: (req, res) => res.render("products/categories",{
+        products: products,
+        category: categories,
+        styles: ["/products/categories"]
+    })
+
 }
