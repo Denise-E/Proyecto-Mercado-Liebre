313cb08b26ca67bc7a8d4647b32810782af53254
Denise Eichenblat <93154485+Denise-E@users.noreply.github.com>
2022-02-08 18:13:49 -0300

    agregando valdiaciones al modelo

--- src/models/users.js
+++ src/models/users.js
@@ -4,8 +4,15 @@ const {body} = require("express-validator");
 
 const model = {
     validate :[
+        body("name").isEmpty().isLength({min:2}),
+        body("lastName").isEmpty().isLength({min:2}),
         body("email").isEmpty().isEmail().withMessage("Invald Email"),
         body("password").isEmpty().isLength({min:5}).withMessage("Min 5 characters"),
+        body("userId").isEmpty().isLength({min:5}).withMessage("Min 5 characters"),
+        body("password").isEmpty().isLength({min:8}).withMessage("Min 8 characters"), // Más valdiaciones
+        body("passwordConfirmation").isEmpty().isLength({min:8}).withMessage("Min 8 characters"), // más validaciones e igualq eu el de arriba
+        body("rol").isIn(["Comprador", "Vendedor"]), // Que sea seleccionado - un select con options
+        body("interests").isIn(["Electronica", "Moda", "Hogar", "Jugueteria", "Vida sana"]) // Que haya uno al menos seleccionado - select con input checkbox 
     ]
 }
 
